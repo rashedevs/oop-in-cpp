66ed52243ac2e2680ddb135eda17a6b93263cce2
rashedevs <rasheduap2015@gmail.com>
2025-07-14 03:23:14 +0600

    different types of inheritance and their modes of access specifier

--- inheritance.cpp
+++ inheritance.cpp
@@ -52,6 +52,46 @@ int main(){
 };
 
 /*
-1. for inheritance constructor, parent constructor called first before child constructor.
-2. for inheritance destructor, child constructor called first to de allocate memory then parent.
+---------------- constructor execution order based on types----------------------
+
+A. for inheritance constructor, parent constructor called first before child constructor.
+B. for inheritance destructor, child constructor called first to de allocate memory then parent.
+
+-----------------modes of inheritance based on access specifier--------------------
+
+1. if we inherits any public property from parent and inherits as a private like,
+   class Student : private Person{......}
+   then the child inherited property turns private into child.
+2. same for protected and public.
+3. private properties of parent class can be inherited in any mode from child.
+4. protected mode properties can be inherited as private or protected, if we inherit any 
+   protected property of parent, in public mode from child. then it will turn into protected
+   automatically.
+5. parent public properties will turn into your mode defined at child class.
+
+--------------------------types of inheritance---------------------------------
+
+1. Single Inheritance.
+
+   Parent (Person) --> Child (student)
+
+2. Multilevel Inheritance.
+
+   Parent (Person) --> Parent (Student) --> Child (Graduate Students - msc/phd)
+
+3. Multiple Inheritance. 
+
+   Parent (Student - name, roll)---~~~]
+                                      |---> Child (TA - name, roll, sub, salary)
+   Parent (Teacher - sub, salary)--~~~]
+
+4. Hierarchial Inheritance.
+
+                                   |-----> Child (Teacher - name, age, sub, salary)
+   Parent (Person - name, age)---|
+                                   |-----> Child (Student - name, age, roll)
+   
+5. Hybrid Inheritance.
+
+   Mixed of multiple types of inheritance alltogether.
 */
